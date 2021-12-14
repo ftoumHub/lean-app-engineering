2d311c44906e472f7de586c98666db7a7590c800
Olivier Francou <olivier.francou@etudiant.univ-lr.fr>
2021-12-14 15:53:50 +0100

    test de modification en cours

--- issue-tracker-ws/src/main/java/org/example/issuetracker/model/IssueStatus.java
+++ issue-tracker-ws/src/main/java/org/example/issuetracker/model/IssueStatus.java
@@ -1,9 +1,20 @@
 package org.example.issuetracker.model;
 
 public enum IssueStatus {
-    NEW,
-    OPEN,
-    ASSIGNED,
-    IN_PROGRESS,
-    DONE
+    NEW("New"),
+    OPEN("Open"),
+    ASSIGNED("Assigned"),
+    IN_PROGRESS("in Progress"),
+    DONE("Done");
+
+    private final String status;
+
+    IssueStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
 }
