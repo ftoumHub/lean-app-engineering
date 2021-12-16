83c29d04e88bcb9c03a2467bb8e53306989d3d20
Olivier Francou <olivier.francou@etudiant.univ-lr.fr>
2021-12-16 12:07:10 +0100

    simplification de l'affichage et pull master

--- issue-tracker-ui/app/IssueRow.js
+++ issue-tracker-ui/app/IssueRow.js
@@ -7,35 +7,14 @@ import IssueDelItem from './IssueDelItem';
 export default class IssueRow extends Component {
   constructor() {
     super();
-    this.state = {
-      showId: false
-    };
-    this.displayId = this.displayId.bind(this);
-    this.hideId = this.hideId.bind(this);
-  }
-
-  displayId() {
-    this.setState({ showId: true })
-  }
-
-  hideId() {
-    this.setState({ showId: false })
   }
 
   render() {
     // On récupère l'attribut issue dans les props histoire de factoriser
-    const { issue } = this.props;
-    let id;
-    if (this.state.showId) {
-      id = <td onClick={this.hideId}><Link to={`/issues/${issue.id}`}>{issue.id}</Link></td>;
-    }
-    else {
-      id = <td onClick={this.displayId}><Link to={`/issues/${issue.id}`}>{issue.id.substring(0, 4)}</Link></td>
-    }
-    
+    const { issue } = this.props; 
     return (
       <tr>
-        {id}
+        <td><Link to={`/issues/${issue.id}`}>{issue.id.substring(0, 4)}</Link></td>
         <td>{issue.status}</td>
         <td>{issue.owner}</td>
         <td>{issue.created.toDateString()}</td>
