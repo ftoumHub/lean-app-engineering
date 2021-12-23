6ef80e7cd1e9ea73e8f08a3a31189e7046b9d8cc
Olivier <76694288+olivierF-Git@users.noreply.github.com>
2021-12-23 11:34:44 +0100

    Feature/test controlleur (#30)
    * ajout des imports
    
    * ajout des imports
    
    * amelioration du test dans les tests json
    
    * tests valides
    
    * Simplification du code et résolution de l'UTF-8
    
    * Ajout de test unitaire
    
    Co-authored-by: Olivier Francou <olivier.francou@etudiant.univ-lr.fr>

---
+++ issue-tracker-ws/src/main/java/org/example/issuetracker/configuration/exception/MyApplicationException.java
@@ -0,0 +1,2 @@
+package org.example.issuetracker.configuration.exception;public class MyApplicationException {
+}
---
+++ issue-tracker-ws/src/main/java/org/example/issuetracker/configuration/handler/MyApplicationExceptionHandler.java
@@ -0,0 +1,2 @@
+package org.example.issuetracker.configuration.handler;public class MyApplicationExceptionHandler {
+}
